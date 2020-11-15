8593db2db82f59cb508ff78af2958c2969bb2905
IsabellaSoares <isabella.soares.28@gmail.com>
2020-11-14 22:32:10 -0300

    finish challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
 
 import "./styles.css";
 
-function App() {
-  async function handleAddRepository() {
-    // TODO
+const App = () => {
+  const [repositories, setRepositories] = useState([])
+
+  const handleAddRepository = async () => {
+    const response = await api.post('/repositories', {
+      title: `Repositório ${Date.now()}`,
+      url: `http://${Date.now()}`,
+      techs: ['React', 'Typescript', 'Node.js']
+    })
+
+    const repository = response.data
+    setRepositories([ ...repositories, repository ])
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  const handleRemoveRepository = async (id) => {
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+    if (repositoryIndex < 0) {
+      alert('Erro ao tentar remover o repositório!')
+      return
+    }
+
+    const response = await api.delete(`/repositories/${id}`)
+
+    if (response.status !== 204) {
+      alert('Erro ao tentar remover o repositório!')
+      return
+    }
+
+    repositories.splice(repositoryIndex, 1)
+    setRepositories([ ...repositories ])
   }
+
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepositories(response.data))
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repo => (
+            <li key={repo.id}>
+              { repo.title }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
